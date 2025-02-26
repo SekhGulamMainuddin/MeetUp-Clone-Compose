0737d7e6b11396fcbbd0b8c88be052d1a06fb460
SekhGulamMainuddin <sekhgmainuddin@gmail.com>
2025-02-26 19:07:08 +0530

    feat: basic textstyles and texts setup

---
+++ app/src/main/java/com/sekhgmainuddin/meetupapp/core/ui/composable/AppTextField.kt
@@ -0,0 +1,89 @@
+package com.sekhgmainuddin.meetupapp.core.ui.composable
+
+import MeetupCloneTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String? = null,
+    customLabel: @Composable (() -> Unit)? = null,
+    singleLine: Boolean = true,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium,
+    labelStyle: TextStyle = MaterialTheme.typography.bodyMedium.copy(
+        color = MaterialTheme.colorScheme.tertiary,
+    ),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    errorText: String? = null,
+) {
+    Column {
+        TextField(
+            modifier = modifier,
+            value = value,
+            textStyle = textStyle,
+            onValueChange = onValueChange,
+            label = customLabel ?: {
+                if (label != null)
+                    Text(text = label, style = labelStyle)
+            },
+            singleLine = singleLine,
+            keyboardActions = keyboardActions,
+            keyboardOptions = keyboardOptions,
+            colors = TextFieldDefaults.colors().copy(
+                focusedIndicatorColor = MaterialTheme.colorScheme.tertiary,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.outlineVariant,
+                disabledIndicatorColor = MaterialTheme.colorScheme.outline,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                cursorColor = MaterialTheme.colorScheme.tertiary,
+            )
+        )
+
+        if (errorText != null)
+            Text(
+                text = errorText,
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    color = MaterialTheme.colorScheme.error,
+                ),
+                modifier = Modifier.padding(top = 8.dp)
+            )
+    }
+}
+
+@Preview
+@Composable
+private fun AppTextFieldPreview(modifier: Modifier = Modifier) {
+    var textFieldValue by remember {
+        mutableStateOf("")
+    }
+    MeetupCloneTheme {
+        AppTextField(
+            value = textFieldValue,
+            label = "Email",
+            onValueChange = {
+                textFieldValue = it
+            },
+        )
+    }
+}
---
+++ app/src/main/java/com/sekhgmainuddin/meetupapp/core/ui/composable/AppTexts.kt
@@ -0,0 +1,346 @@
+package com.sekhgmainuddin.meetupapp.core.ui.composable
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+
+@Composable
+fun BodyLargeText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.bodyLarge,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun BodyMediumText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.bodyMedium,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun BodySmallText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.bodySmall,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun TitleLargeText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.titleLarge,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun TitleMediumText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.titleMedium,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun TitleSmallText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.titleSmall,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun HeadlineLargeText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineLarge,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun HeadlineMediumText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineMedium,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun HeadlineSmallText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineSmall,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun LabelLargeText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelLarge,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun LabelMediumText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelMedium,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun LabelSmallText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelSmall,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+
+@Composable
+fun DisplayLargeText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelLarge,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun DisplayMediumText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelMedium,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
+
+@Composable
+fun DisplaySmallText(
+    modifier: Modifier = Modifier, text: String,
+    color: Color? = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    decoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int? = Int.MAX_VALUE,
+) {
+    Text(
+
+        text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelSmall,
+        color = color ?: Color.Unspecified,
+        textAlign = textAlign,
+        textDecoration = decoration,
+        overflow = overflow,
+        maxLines = maxLines ?: Int.MAX_VALUE
+    )
+}
